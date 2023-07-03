c5578baff215e41fbb19d523f846790a7880acb0
han century <han2050339157@gmail.com>
2023-07-03 15:01:23 +0800

    1234

--- src/main/java/com/music/servlet/auth/Indexservlet.java
+++ src/main/java/com/music/servlet/auth/Indexservlet.java
@@ -28,7 +28,6 @@ public class Indexservlet extends HttpServlet {
         context.setVariable("username",user.getUsername());
         context.setVariable("music_List",service.getMusicList());
         ThymeleafUtil.process("index.html",context,resp.getWriter());
-        System.out.println("last one");
     }
 
 }
