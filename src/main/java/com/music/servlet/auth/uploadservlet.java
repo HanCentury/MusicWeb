2fa343cf3bfa3950fe33cb5223dbe57302a9b68a
han century <han2050339157@gmail.com>
2023-07-18 11:29:58 +0800

    right

--- src/main/java/com/music/servlet/auth/uploadservlet.java
+++ src/main/java/com/music/servlet/auth/uploadservlet.java
@@ -32,7 +32,7 @@ public class uploadservlet extends HttpServlet {
         user user= (user) req.getSession().getAttribute("user");
         context.setVariable("username",user.getUsername());
 
-
+        System.out.println();
         ThymeleafUtil.process("upload.html",context,resp.getWriter());
     }
     @Override
